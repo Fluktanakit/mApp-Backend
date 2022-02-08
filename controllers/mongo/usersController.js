c03ca7302ebe118eb9b7ef9b349f998a724054d7
Fluktanakit <tanakit_ke62@live.rmutl.ac.th>
2022-02-08 22:23:47 +0700

    week 12

--- controllers/mongo/usersController.js
+++ controllers/mongo/usersController.js
@@ -1,13 +1,14 @@
-const users = require("../../models/mongo/users.js")
+const users = require("../../models/mongo/users.js");
 
 exports.insert = async (req, res, next) => {
     let data = new users({
-        email:"tanakit421711@gmail.com",
-        name: "tanakit",
-        lastname:"Sleep"
-    })
-    data.save()
+        email: "tonmorakot@gmail.com",
+        name: "มรกต",
+        lastname: "ทองพรหม"
+    });
+
+    data.save();
     res.status(200).json({
-        message:"บันทึกข้อมูลเรียบร้อย"
-    })
+        message: "บันทึกข้อมูลเรียบร้อยแล้ว"
+    });
 }
